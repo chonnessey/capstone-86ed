362a661a53c50133aa1ccf66f9a0c611ac71c854
Adrian Camacho <adriancamacho18@gmail.com>
2021-10-01 14:45:14 -0700

    install emotion react from material ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
 
 const Header = () => {
   return (
     <div className="header">
       <div className="header-left">
         <img src="https://pbs.twimg.com/profile_images/1328442491843059717/8i9075yw_400x400.jpg" alt="86 logo"/>
+        <div className="header-input">
+          <SearchIcon />
+        </div>
       </div>
       <div className="header-middle"></div>
       <div className="header-right"></div>
