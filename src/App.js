9aee003e5735e4183602942a0772fa6d5d921ffa
Adrian Camacho <adriancamacho18@gmail.com>
2021-10-01 14:33:56 -0700

    add initial styling for header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Eighty Sixed</h1>
+      {/* App Body */}
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-left">
+        <img src="https://pbs.twimg.com/profile_images/1328442491843059717/8i9075yw_400x400.jpg" alt="86 logo"/>
+      </div>
+      <div className="header-middle"></div>
+      <div className="header-right"></div>
+    </div>
+  )
+}
+
+export default Header;
