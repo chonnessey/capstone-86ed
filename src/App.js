8518dc871c60c12bb18db902dcc2c5cfaa7a44eb
Adrian Camacho <adriancamacho18@gmail.com>
2021-10-01 13:52:23 -0700

    remove boilerplate files and add firebase config to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Eighty Sixed</h1>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDMbpYYoqv-ktV5IUbrWMq_zW4VW2JuDZs",
+  authDomain: "eighty-sixed-76cf1.firebaseapp.com",
+  projectId: "eighty-sixed-76cf1",
+  storageBucket: "eighty-sixed-76cf1.appspot.com",
+  messagingSenderId: "666872075005",
+  appId: "1:666872075005:web:9c03d9fb02b08fb45f2494"
+};
